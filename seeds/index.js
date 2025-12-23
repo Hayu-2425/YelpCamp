45afe4e653d838e919dd845478df84483da2e170
Hayat Ahmed <hayathamid693@gmail.com>
2025-12-23 19:10:38 +0300

    Finish basic styles and layouts bootstrap

---
+++ seeds/index.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+//To delete data saved in db before and save one data in db
+// const seedDB = async() => {
+//      await Campground.deleteMany({});
+//      const c = new Campground({ title: "purple field"})
+//      await c.save();
+
+// }
+// seedDB();
+
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+//To delete data saved in db before and save many data in db
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
+        await camp.save();
+    }
+}
+
+
+//To close our db connection
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
